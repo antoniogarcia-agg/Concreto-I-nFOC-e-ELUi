bb7b211a9913b7a90fe789cfcc206f1b7ebdaea9
Antônio Garcia <73239671+antoniogarcia-agg@users.noreply.github.com>
2025-12-04 16:35:56 -0300

    Add files via upload

---
+++ plota_teq.m
@@ -0,0 +1,45 @@
+%% Plotagem da trajetória de equilíbrio para uma excentricidade fixa | Concreto I | Antônio Garcia
+function plota_teq(classe_concreto, classe_aco, gamac, gamas, e, diametro_aco, x, y, xs, ys, m, z, prec)
+    
+    [sigmacd, ~, ~, ~, fyd, ~] = parametros(classe_concreto, classe_aco, gamac, gamas);
+    A_c = Ac(x, y);
+    Ast = sum(pi * diametro_aco.^2 / 4);
+    Nmax = sigmacd*A_c+fyd*Ast;
+
+    % Inicialização do algoritmo
+    n_passos = 10;
+    deltaN = Nmax/n_passos;
+    i = 1;
+    Nd(i) = 0;
+    Md(i) = 0;
+    N = 0;
+    tol = 1e-10;
+    cont = 1;
+
+    while deltaN >= tol
+        for i=2:n_passos
+            cont = cont + 1;
+            N = N +deltaN;
+            [verificacao, f, ~, ~, ~] = verifica_pilar(classe_concreto, classe_aco, gamac, gamas, N, e*N, diametro_aco, x, y, xs, ys, m, z, prec);
+            if verificacao == true
+                Nd(cont) = N;
+                Md(cont) = N*(e+f);
+                Ncr = Nd(cont);
+            else
+                break
+            end
+        end
+        cont = cont-1;
+        N = N-deltaN;
+        deltaN = deltaN/10;
+    end
+
+    fprintf("Para a excentricidade %f m, a carga crítica é %f NM.", e, Ncr);
+    % Plotagem  
+    figure;
+    plot(Nd(1:cont), Md(1:cont));
+    xlabel('Força normal');
+    ylabel('Momento');
+    title('Trajetória de equilíbrio para uma excentricidade fixa');
+    grid on;
+end
---
+++ q2c_exame_2024.m
@@ -0,0 +1,22 @@
+% Materiais
+classe_concreto = 65;
+classe_aco = 50;
+gamac = 1.4;
+gamas = 1.15;
+
+% Esforços
+e = 2/103;
+
+% Geometria
+x = [0; 0.2; 0.2; 0; 0];
+y = [0; 0; 0.2; 0.2; 0];
+xs = [0.03; 0.03; 0.03; 0.17; 0.17; 0.17];
+ys = [0.03; 0.1; 0.17; 0.03; 0.1; 0.17];
+diametro_aco = [0.01; 0.01; 0.01; 0.01; 0.01; 0.01];
+z = 2;
+
+% Algoritmo
+m = 100;
+prec = 1e-10;
+
+plota_teq(classe_concreto, classe_aco, gamac, gamas, e, diametro_aco, x, y, xs, ys, m, z, prec);
