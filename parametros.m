fc6d6906abf9d7d653c9ef63edbd39c319569b81
Antônio Garcia <73239671+antoniogarcia-agg@users.noreply.github.com>
2026-06-27 11:20:58 -0300

    Add files via upload

--- parametros.m
+++ parametros.m
@@ -3,12 +3,12 @@
 function [sigmacd,epsilonc2,epsiloncu,n, fyd, epsilonyd] = parametros(classe_concreto, classe_aco, gamac, gamas)
     % Parâmetros do concreto
     fck = classe_concreto;
-    eta_c = 1; %fator de correção do efeito Rusch
+    eta_c = 1;
     if (fck>40)
         eta_c=(40/fck)^(1/3);
     end
 
-    sigmacd = eta_c*0.85*fck/gamac; % Multiplicar por eta_c para considerar o efeito rüsch
+    sigmacd = eta_c*0.85*fck/gamac;
     n = 2;
     epsilonc2 = 2;
     epsiloncu = 3.5;
